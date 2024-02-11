3598ec57be12f2c9d2677282fd45a07efb7ae798
VRAJ_CLERK <vrajclerk04@gmail.com>
2024-02-11 22:42:48 +0530

    todo-redux

---
+++ reduxtoolkitTodo/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todos'
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+      <h1> learn redux</h1>
+      <AddTodo />
+      <Todos />
+      </div>
+    </>
+  )
+}
+
+export default App
